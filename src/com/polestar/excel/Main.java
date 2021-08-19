5b2b17fec84271af6577cebf7ee9896f01237780
sook8878 <qpwlxk123@gmail.com>
2021-08-19 23:27:15 +0900

    excel task done

--- src/com/polestar/excel/Main.java
+++ src/com/polestar/excel/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         StudyListToExcel studyListToExcel = new StudyListToExcel();
-        studyListToExcel.MakeExcel("excelTask.xlsx");
+        studyListToExcel.MakeExcel("excelTask2.xlsx");
 
     }
 }
